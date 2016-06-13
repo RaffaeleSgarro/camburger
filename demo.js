9090660b00047d7215d62d9af02931e4694fcdf8
Raffaele Sgarro <raffaeleSgarro@gmail.com>
2016-06-13 22:20:46 +0200

    TimeZones in demo menu

--- demo.js
+++ demo.js
@@ -1,39 +1,18 @@
 $(function() {
     var menu = [];
     var i;
-    
-    appendItems(25, menu);
-    
-    $.each(menu, function(i, item){
-        appendItems(10, item.children);
-    });
-    
-    function appendItems(howManyItems, children) {
-        for (i = 0; i < howManyItems; i++) {
-            item  = {
-                icon: "flag",
-                id: i,
-                title: "Item #" + i,
-                terms: [],
-                favourite: false,
-                children: []
-            };
-            
-            children.push(item);
-        }
-    }
-    
+
     window.sidebar = new Camburger.Sidebar({});
-    sidebar.setMenu(menu);
-    
+    sidebar.setMenu(window.tz.children);
+
     $('.toggle-sidebar').click(function(e){
         window.sidebar.show();
     });
-    
+
     sidebar.subscribe('item:click', function(ctx, item){
         console.log('Clicked ' + item.title);
     });
-    
+
     $(document).keypress(function(e){
         if (e.key == ' ' && e.ctrlKey) {
             window.sidebar.toggle();
